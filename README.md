77ed0679065b9ae981fd61573b50314b3db854ce
hendisantika <hendisantika@yahoo.co.id>
2019-04-21 17:22:24 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-hris-sample1
---
+++ src/main/java/com/hendisantika/hris/springboothrissample1/SpringbootHrisSample1Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.hris.springboothrissample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHrisSample1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHrisSample1Application.class, args);
+    }
+
+}
