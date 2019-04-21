03f5bb70e9bd03444f2a9d1536388c288e2a2c9c
hendisantika <hendisantika@yahoo.co.id>
2019-04-21 18:27:26 +0700

    #20 - Add UserDTO

---
+++ src/main/java/com/hendisantika/hris/springboothrissample1/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.hendisantika.hris.springboothrissample1.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-hris-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-21
+ * Time: 18:27
+ */
+@Data
+public class UserDTO {
+    private String username;
+    private String password;
+    private String email;
+
+}
