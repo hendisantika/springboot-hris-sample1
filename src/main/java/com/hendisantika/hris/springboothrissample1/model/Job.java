e8d843c750cce7a6cda49125823d6997a7e9f7d0
hendisantika <hendisantika@yahoo.co.id>
2019-04-21 17:31:02 +0700

    #8 - Add Job Model

---
+++ src/main/java/com/hendisantika/hris/springboothrissample1/model/Job.java
@@ -0,0 +1,33 @@
+package com.hendisantika.hris.springboothrissample1.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-hris-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-21
+ * Time: 17:30
+ */
+@Entity
+@Table(name = "JOBS")
+public class Job {
+    @Id
+    @Column(name = "JOB_ID")
+    private String jobId;
+
+    @Column(name = "JOB_TITLE")
+    private String JobTitle;
+
+    @Column(name = "MIN_SALARY")
+    private BigDecimal minSalary;
+
+    @Column(name = "MAX_SALARY")
+    private BigDecimal maxSalary;
+}
