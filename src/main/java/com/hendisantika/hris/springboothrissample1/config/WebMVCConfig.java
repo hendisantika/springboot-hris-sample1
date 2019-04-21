3df9232b6dffae41889bbc9195699b02704c58ac
hendisantika <hendisantika@yahoo.co.id>
2019-04-21 18:21:52 +0700

    #16 - Add WebMVCConfig

---
+++ src/main/java/com/hendisantika/hris/springboothrissample1/config/WebMVCConfig.java
@@ -0,0 +1,26 @@
+package com.hendisantika.hris.springboothrissample1.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-hris-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-21
+ * Time: 18:19
+ */
+@Configuration
+public class WebMVCConfig implements WebMvcConfigurer {
+    @Autowired
+    SecurityInterceptor securityInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(securityInterceptor);
+    }
+}
