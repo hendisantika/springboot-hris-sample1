cd3040fc20e9c97ad7e51c146fd4ad60cd1b9656
hendisantika <hendisantika@yahoo.co.id>
2019-04-23 05:53:44 +0700

    #35 - Add HrController

---
+++ src/main/java/com/hendisantika/hris/springboothrissample1/controller/HrController.java
@@ -0,0 +1,35 @@
+package com.hendisantika.hris.springboothrissample1.controller;
+
+import com.hendisantika.hris.springboothrissample1.dto.UserSessionBean;
+import com.hendisantika.hris.springboothrissample1.service.DepartmentService;
+import com.hendisantika.hris.springboothrissample1.service.EmployeeService;
+import com.hendisantika.hris.springboothrissample1.service.MiscService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-hris-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-23
+ * Time: 05:53
+ */
+@Controller
+public class HrController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private MiscService misc;
+
+    @Autowired
+    private DepartmentService deptServ;
+
+    @Autowired
+    private com.hendisantika.hris.springboothrissample1.service.UserService UserService;
+
+    @Autowired
+    private UserSessionBean currentUser;
+}
